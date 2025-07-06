f3af5c2ff7473a8565d513c046cf891944b86269
Om Panpatil <ompanpatil513@gmail.com>
2025-07-06 17:44:30 +0530

    Conquer the web

---
+++ CP/JS_/Intro and basics/Basic/script.js
@@ -0,0 +1,5 @@
+console.log("Conquer the web with JS!");
+console.warn("Warning : Once u started learning JS, u will never stop learning it!");
+console.error("Error : JS is not a programming language, it is a scripting language and practice it daily to become a pro!");
+
+
