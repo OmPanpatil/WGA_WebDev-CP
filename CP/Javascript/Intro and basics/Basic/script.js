5fa0e66eff993deaf1c6e705bfe72968d4cd827c
Om Panpatil <ompanpatil513@gmail.com>
2025-07-23 14:45:03 +0530

    JS

--- CP/Javascript/Intro and basics/Basic/script.js
+++ CP/Javascript/Intro and basics/Basic/script.js
@@ -2,4 +2,23 @@ console.log("Conquer the web with JS!");
 console.warn("Warning : Once u started learning JS, u will never stop learning it!");
 console.error("Error : JS is not a programming language, it is a scripting language and practice it daily to become a pro!");
 
+var a = 12;
+var b = 13;
+console.log("Heyy!");
+console.log(a+b);
+
+// Browser Inbuilt Feature : Console,Prompt, alert
+// Console --> log, warn, error
+console.log("Hello");
+console.warn("Warning : Once u started learning JS, u will never stop learning it!");
+console.error("Error : JS is not a programming language, it is a scripting language and practice it daily to become a pro!");
+
+// alert('HEyyy!');
+
+// Prompt: Alert ka bhai hai jo ki aek kadam agae hai jismae hum puch sakhte hai user sae.
+
+var nam = prompt("Naam batao")
+;
+console.log(nam);
+
 
