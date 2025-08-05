f28b668d44cc93550ea6e9233fa7b5e9e078cbdf
Om Panpatil <ompanpatil513@gmail.com>
2025-08-05 15:16:24 +0530

    JS

---
+++ CP/Javascript/DSA/Basic/script.js
@@ -0,0 +1,49 @@
+console.log("1" - 1);
+console.log(5/4);
+console.log(Math.floor(5/4));   //To avoid the fractions or float 
+console.log(Math.ceil(10)); // to the higher number.
+console.log(Math.floor(9.8));   //To the lower number.
+
+console.log("Math round function : ");
+console.log(Math.round(10.4)); //To the lower number floor
+console.log(Math.round(10.5));
+console.log(Math.round(10.6)); // To the higher number as ceil
+
+console.log(Math.abs(-8));
+
+console.log(Math.trunc(43.785925890285938));
+
+console.log(Math.pow(4,2));
+
+console.log(Math.sqrt(40));
+console.log(Math.trunc(Math.sqrt(40)));
+
+console.log(Math.cbrt(125));
+
+console.log(Math.cbrt(7));
+
+console.log(Math.max(7,10,80));
+console.log(Math.min(7,10,80));
+
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+
+let a = 7593.423
+console.log(Number(a.toFixed(3)));
+
+// Q. Compound interest 
+
+// let p = Number(prompt("Enter ur principal amount : "));
+// let r = Number(prompt("Enter ur rate : "));
+// let t = Number(prompt("Enter a time : "));
+
+// A = P*(1+r/100)^t
+// Cp = A - P
+
+// console.log((p*Math.pow(1+r/100, t)) - p);
+
+console.log(Math.floor(Math.random()*9000 + 1000));   //1000 - 9999
+
+
+
