a59f45df5e0921a3adbe47e10c61af23a25bb43e
Om Panpatil <ompanpatil513@gmail.com>
2025-08-07 15:40:05 +0530

    DOM JS

--- WebDev/DOM by Sarthak sensei/First lec/script.js
+++ WebDev/DOM by Sarthak sensei/First lec/script.js
@@ -7,4 +7,32 @@ head1.addEventListener("click",function(){
     console.log("heyy");
     head1.textContent = "Heyy";
     head1.style.color = "Orangered";
+});
+
+
+const yourevent = new Event("ApnaEvent");
+let btn = document.createElement('button');
+btn.textContent = "Ur Event!";
+document.querySelector("body").appendChild(btn);
+// const yourevent = new Event("ApnaEvent");
+btn.addEventListener("click", function(){
+    btn.dispatchEvent(yourevent);
 })
+btn.addEventListener("ApnaEvent", function(){
+    alert("Tumhara  event");
+    console.log("Tumhari party");
+    
+    
+});
+// document.querySelector("button").dispatchEvent(yourevent);
+
+
+let box = document.createElement('div');
+box.classList.add('box-inner');
+document.querySelector("body").appendChild(box);
+
+let torches = document.querySelector('torch');
+torches.style.height = '5vh';
+torches.style.width = '5vw';
+torches.style.backgroundColor = 'lightcoral';
+// torches.style.position = 'absolute';
