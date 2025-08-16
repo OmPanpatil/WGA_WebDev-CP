27fd0dec6d6bbaa4489df7b03a0b49455995a8f9
Om Panpatil <ompanpatil513@gmail.com>
2025-08-16 14:54:32 +0530

    JS

---
+++ WebDev/GSAP/Gsap_Projects/Landpage2/script.js
@@ -0,0 +1,19 @@
+// let tl = gsap.timeline();
+
+gsap.from("nav #logo", {
+    y:-60,
+    delay:0.2,
+    opacity:0,
+    duration:1,
+    // stagger:0.4
+    // scrub: 2
+});
+
+gsap.from("nav h3", {
+    y:-60,
+    delay:0.2,
+    opacity:0,
+    duration:1,
+    // stagger:0.4
+    // scrub:2
+});
